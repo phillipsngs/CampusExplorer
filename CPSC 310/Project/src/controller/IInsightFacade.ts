181458a2a60f716d5cfe88c3cdea0e523e5eb51a
Phill <phillipsngs@gmail.com>
2021-09-14 14:10:32 -0700

    Added some of the configuration files

--- CPSC 310/Project/src/controller/IInsightFacade.ts
+++ CPSC 310/Project/src/controller/IInsightFacade.ts
@@ -1,2 +1,3 @@
 /*The contents of InsightFacade.ts must include an InsightFacade class that is the default export
-of the file and implements IInsightFacade. */
+of the file and implements IInsightFacade.
+ 122400eec96dc95513cf5f125c6803c008c69701*/

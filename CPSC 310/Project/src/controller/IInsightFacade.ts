138224fedc3275fa32464bcce58abfd27e18c2ee
Phill <phillipsngs@gmail.com>
2023-05-10 21:09:31 -0700

    init

--- CPSC 310/Project/src/controller/IInsightFacade.ts
+++
@@ -1,3 +0,0 @@
-/*The contents of InsightFacade.ts must include an InsightFacade class that is the default export
-of the file and implements IInsightFacade.
- 122400eec96dc95513cf5f125c6803c008c69701*/
